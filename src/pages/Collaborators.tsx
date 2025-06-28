101fbb47f2345f722e0e8834a3c958a1bb0acb22
Ali Ranjah <aliranjah@Alis-MacBook-Air.local>
2025-06-28 23:06:28 +0100

    updated 'apply now' and 'request access' to display all information into a google sheet with tally.

--- src/pages/Collaborators.tsx
+++ src/pages/Collaborators.tsx
@@ -155,9 +155,12 @@ const Collaborators: React.FC = () => {
               <div className="relative group">
                 <span className="absolute inset-0 rounded-lg p-[2px] bg-gradient-to-r from-purple-200 via-purple-300 to-purple-400 blur-sm opacity-70 group-hover:opacity-100 animate-gradient-x z-0" />
                 <button
+                  type="button"
+                  data-tally-open="31YM8p"
+                  data-tally-layout="modal"
+                  data-tally-emoji-animation="none"
                   className="relative flex items-center justify-center py-4 px-8 rounded-lg bg-gradient-to-r from-purple-600 to-purple-700 text-white font-bold text-lg transition-all duration-200 hover:scale-105 hover:-translate-y-1 shadow-lg border border-transparent whitespace-nowrap z-10 font-baloo overflow-hidden"
                   style={{minWidth: '200px'}}
-                  onClick={() => setIsApplicationFormOpen(true)}
                 >
                   <span className="relative flex items-center z-10">
                     {language === "fr" ? "Postuler Maintenant" : "Apply Now"}
